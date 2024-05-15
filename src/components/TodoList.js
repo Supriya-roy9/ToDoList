09f395b9a08fe77b4da16393578b1cef0c5619de
Supriya Roy <supriyaroy.982000@gmail.com>
2024-05-15 17:56:19 +0530

    Checkpoint 3 completed

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
 function TodoList() {
+  const todoItems = [
+    "Read SpringBoot",
+    "Complete assignments",
+    "Prepare breakfast",
+    "Sleep for 2 hours",
+    "Take a shower"
+  ];
+
   return (
     <div className="todo-list">
-      <p>Nothing to do buddy. Sleep!!</p>
+      {todoItems.map((item, index) => (
+        <div key={index} className="todo-item">
+          {item}
+        </div>
+      ))}
     </div>
   );
 }
 
 export default TodoList;
+
