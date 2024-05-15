f2c5b47f4e6270733b5ce91de42c9471b45a690f
Supriya Roy <supriyaroy.982000@gmail.com>
2024-05-15 18:07:49 +0530

    Checkpoint 4 completed

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function TodoList() {
-  const todoItems = [
+  const [todoItems, setTodoItems] = useState([
     "Read SpringBoot",
     "Complete assignments",
     "Prepare breakfast",
     "Sleep for 2 hours",
     "Take a shower"
-  ];
+  ]);
+
+  const handleEmptyButtonClick = () => {
+    setTodoItems([]);
+  };
 
   return (
     <div className="todo-list">
-      {todoItems.map((item, index) => (
-        <div key={index} className="todo-item">
-          {item}
-        </div>
-      ))}
+      {todoItems.length === 0 ? (
+        <p><em>Nothing to do buddy. Sleep!</em></p>
+      ) : (
+        todoItems.map((item, index) => (
+          <div key={index} className="todo-item">
+            {item}
+          </div>
+        ))
+      )}
+      {todoItems.length > 0 && (
+        <button onClick={handleEmptyButtonClick}>Empty</button>
+      )}
     </div>
   );
 }
 
 export default TodoList;
-
